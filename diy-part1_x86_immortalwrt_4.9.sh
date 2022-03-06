aee59ae9384e4879c1fc53112ec63459e022a113
kissyouhunter <63334206+kissyouhunter@users.noreply.github.com>
2022-03-06 16:34:40 +0800

    Update diy-part1_x86_immortalwrt_4.9.sh

--- diy-part1_x86_immortalwrt_4.9.sh
+++ diy-part1_x86_immortalwrt_4.9.sh
@@ -31,6 +31,6 @@ sed -i 's/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=4.9/g' ./target/linux/x86/Makef
 
 # 内核替换成 kernel 4.9.299
 
-sed -i 's/LINUX_KERNEL_HASH-4.9.297 = e43ba8cde809b3126a5192f5a8a83f47134a138955ce6811672f5f704db6d89f/LINUX_KERNEL_HASH-4.9.302 = d5456f05050e323d996721975bfe2b8bb1143ce68a4e5ae84e1f922f37b02e36/g' ./include/kernel-version.mk
+#sed -i 's/LINUX_KERNEL_HASH-4.9.297 = e43ba8cde809b3126a5192f5a8a83f47134a138955ce6811672f5f704db6d89f/LINUX_KERNEL_HASH-4.9.302 = d5456f05050e323d996721975bfe2b8bb1143ce68a4e5ae84e1f922f37b02e36/g' ./include/kernel-version.mk
 
-sed -i 's/LINUX_VERSION-4.9 = .297/LINUX_VERSION-4.9 = .302/g' ./include/kernel-version.mk
+#sed -i 's/LINUX_VERSION-4.9 = .297/LINUX_VERSION-4.9 = .302/g' ./include/kernel-version.mk
