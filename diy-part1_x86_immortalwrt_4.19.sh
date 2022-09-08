0a1a1352f8477fa3c681df9b3b3f6ff018302cd7
kissyouhunter <kissyouhunter@hotmail.com>
2022-09-08 13:20:28 +0800

    Update diy-part1_x86_immortalwrt_4.19.sh

--- diy-part1_x86_immortalwrt_4.19.sh
+++ diy-part1_x86_immortalwrt_4.19.sh
@@ -30,6 +30,6 @@ wget -O ./package/emortal/default-settings/files/openwrt_banner https://raw.gith
 
 # 内核替换成 kernel 4.19.235
 
-#sed -i 's/LINUX_KERNEL_HASH-4.19.231 = 673123b3b3b05e2d8586c82a342ff2556e0b1f2caaadd643fefb9d91d58d8131/LINUX_KERNEL_HASH-4.19.235 = 530c5ac848111bbf7d1ad407a4ce8173ef8f9a4554477a32c695c5a4eaf02598/g' ./include/kernel-version.mk
+sed -i 's/LINUX_KERNEL_HASH-4.19.246 = 00ad2f5a36c91221a2ade0078b93bf84b60d494bd1ef51eaccb5bdb6277dba3a/LINUX_KERNEL_HASH-4.19.257 = eadd13aa70f37cdf50eef45c5964bd7146d353b61a1fd026d4fa0b2a68a3ea47/g' ./include/kernel-version.mk
 
-#sed -i 's/LINUX_VERSION-4.19 = .231/LINUX_VERSION-4.19 = .235/g' ./include/kernel-version.mk
+sed -i 's/LINUX_VERSION-4.19 = .246/LINUX_VERSION-4.19 = .257/g' ./include/kernel-version.mk
